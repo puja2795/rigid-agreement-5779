696b25dc3fe1eddd136f93cbc8f0ab2445f47477
puja2795 <pujarinisahoo95@gmail.com>
2022-12-15 19:31:56 +0530

    navbar-subnavbar

--- jio-mart/src/App.js
+++ jio-mart/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import SubNavbar from './components/SubNavbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SubNavbar />
     </div>
   );
 }
--- jio-mart/src/components/Navbar.jsx
+++ jio-mart/src/components/Navbar.jsx
@@ -1,101 +1,63 @@
-import { ReactNode } from 'react';
 import {
+  Icon,
+  Text,
   Box,
+  Image,
   Flex,
-  Avatar,
-  HStack,
+  InputGroup,
+  Input,
+  InputRightElement,
   Link,
-  IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
   useDisclosure,
   useColorModeValue,
   Stack,
-} from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { MdFormatListBulleted } from "react-icons/md";
+import { FaUser, FaShoppingCart } from "react-icons/fa";
 
-const Links = ['Dashboard', 'Projects', 'Team'];
-
-const NavLink = ({ children }) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={'md'}
-    _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
-    }}
-    href={'#'}>
-    {children}
-  </Link>
-);
 
 export default function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <IconButton
-            size={'md'}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={8} alignItems={'center'}>
-            <Box>Logo</Box>
-            <HStack
-              as={'nav'}
-              spacing={4}
-              display={{ base: 'none', md: 'flex' }}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </HStack>
-          </HStack>
-          <Flex alignItems={'center'}>
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
-              </MenuList>
-            </Menu>
+      <Box bg="#008ecc" color="white" px={4} h={20} textAlign='center'>
+        <Box w='70%' pt='2' m="auto">
+          <Flex h={16} alignItems={"center"} justifyContent={"space-between"} gap='8'>
+            <HamburgerIcon boxSize={10} />
+            <Image
+              src="https://www.jiomart.com/assets/version1669919139/smartweb/images/jiomart_logo_beta.svg"
+              alt="logo" h='41'
+            />
+            <InputGroup width="60%" margin="auto">
+              <Input
+                bg="white"
+                placeholder="Search essentials, groceries, and more...  "
+              />
+              <InputRightElement
+                color="black"
+                children={<Icon boxSize={5} as={MdFormatListBulleted} />}
+              />
+            </InputGroup>
+            <Flex gap={10}>
+              <Flex alignItems={"center"} gap={2}>
+                <Icon boxSize={5} as={FaUser} />
+                {/* <Text fontSize="lg">Sign in / Sign up</Text> */}
+                <Text fontSize="md" as="b">
+                  Sign in / Sign up
+                </Text>
+              </Flex>
+              <Flex alignItems={"center"} gap={2}>
+                <Icon boxSize={5} as={FaShoppingCart} />
+                <Text fontSize="md" as="b">
+                  Cart
+                </Text>
+              </Flex>
+            </Flex>
           </Flex>
-        </Flex>
-
-        {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </Stack>
-          </Box>
-        ) : null}
+        </Box>
       </Box>
-
-      <Box p={4}>Main Content Here</Box>
     </>
   );
 }
